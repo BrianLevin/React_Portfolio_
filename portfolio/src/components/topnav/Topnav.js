5e959407ae2c3e2d301f02d60e66c13e3cdc343b
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2021-12-15 09:28:43 -0500

    added scss styling and material ui-icons

--- portfolio/src/components/topnav/Topnav.js
+++ portfolio/src/components/topnav/Topnav.js
@@ -1,9 +1,28 @@
 import "./topnav.scss"
-
+import { Person, Mail } from "@material-ui/icons"
 export default function Topnav() {
     return (
         <div className="topnav">
+            < div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">genius.</a>
+                    <div className="itemContainer">
+                        <Person className="icon" />
+                        <span>#443434334344</span>
 
+                    </div>
+
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>Brian@gmail.com</span>
+
+                    </div>           </div>
+
+                <div className="right">
+
+                </div>
+
+            </div>
         </div>
     )
 }
