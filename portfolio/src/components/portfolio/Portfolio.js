bfd8475929a094515e04b1f30fe15541d3613277
BrianLevin <bml201095@gmail.com>
2021-12-16 21:58:20 -0500

    made some corrections and formatted document

--- portfolio/src/components/portfolio/Portfolio.js
+++ portfolio/src/components/portfolio/Portfolio.js
@@ -1,41 +1,54 @@
 import "./portfolio.scss";
 
 export default function Portfolio() {
-    return (
-        <div className="portfolio" id="portfolio">
-            <h1>Portfolio</h1>
-            <ul>
-<li className="active">Featured</li>
-<li>Web App</li>
-<li>Mobile App</li>
-<li>Design</li>
-<li>Branding</li>
-
-            </ul>
-            <div class="container">
-<div class="item">
-    <img src="https://www.techfunnel.com/wp-content/uploads/2019/07/12-Digital-Banking-Challenges-and-Opportunities-For-the-Banking-Industry-1.png" alt="" />
-    <h3> Banking App</h3>
-</div>
-<div class="item">
-    <img src="https://www.techfunnel.com/wp-content/uploads/2019/07/12-Digital-Banking-Challenges-and-Opportunities-For-the-Banking-Industry-1.png" alt="" />
-    <h3> Banking App</h3>
-</div>
-
-<div class="item">
-    <img src="https://www.techfunnel.com/wp-content/uploads/2019/07/12-Digital-Banking-Challenges-and-Opportunities-For-the-Banking-Industry-1.png" alt="" />
-    <h3> Banking App</h3>
-</div>
-<div class="item">
-    <img src="https://www.techfunnel.com/wp-content/uploads/2019/07/12-Digital-Banking-Challenges-and-Opportunities-For-the-Banking-Industry-1.png" alt="" />
-    <h3> Banking App</h3>
-</div>
-<div class="item">
-    <img src="https://www.techfunnel.com/wp-content/uploads/2019/07/12-Digital-Banking-Challenges-and-Opportunities-For-the-Banking-Industry-1.png" alt="" />
-    <h3> Banking App</h3>
-</div>
-            </div>
-        
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div class="container">
+        <div class="item">
+          <img
+            src="https://www.techfunnel.com/wp-content/uploads/2019/07/12-Digital-Banking-Challenges-and-Opportunities-For-the-Banking-Industry-1.png"
+            alt=""
+          />
+          <h3> Banking App</h3>
         </div>
-    )
+        <div class="item">
+          <img
+            src="https://www.techfunnel.com/wp-content/uploads/2019/07/12-Digital-Banking-Challenges-and-Opportunities-For-the-Banking-Industry-1.png"
+            alt=""
+          />
+          <h3> Banking App</h3>
+        </div>
+
+        <div class="item">
+          <img
+            src="https://www.techfunnel.com/wp-content/uploads/2019/07/12-Digital-Banking-Challenges-and-Opportunities-For-the-Banking-Industry-1.png"
+            alt=""
+          />
+          <h3> Banking App</h3>
+        </div>
+        <div class="item">
+          <img
+            src="https://www.techfunnel.com/wp-content/uploads/2019/07/12-Digital-Banking-Challenges-and-Opportunities-For-the-Banking-Industry-1.png"
+            alt=""
+          />
+          <h3> Banking App</h3>
+        </div>
+        <div class="item">
+          <img
+            src="https://www.techfunnel.com/wp-content/uploads/2019/07/12-Digital-Banking-Challenges-and-Opportunities-For-the-Banking-Industry-1.png"
+            alt=""
+          />
+          <h3> Banking App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
