360f1dcc33017b76f35489bb55eb528cc4b0b020
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2021-12-21 10:11:08 -0500

    finished the work section by adding cvard slider

--- portfolio/src/components/work/Work.js
+++ portfolio/src/components/work/Work.js
@@ -1,29 +1,79 @@
+import { mobilePortfolio } from "../../data";
 import "./work.scss";
 
+import React, { useState } from "react";
 export default function Work() {
+const[currentSlide,setCurrentSlide] = useState(0);
+
+const data =[
+  {
+    id: "1",
+    icon: "./assets/mobile.png",
+    title: "Web Design",
+    desc:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+    img: "https://neilpatel.com/wp-content/uploads/2021/08/social-media-metrics.jpg",
+},
+{
+  id: "2",
+  icon: "./assets/globe.png",
+  title: "Mobile Application",
+  desc:
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+  img: "https://neilpatel.com/wp-content/uploads/2021/08/social-media-metrics.jpg",
+},
+
+{
+  id: "3",
+  icon: "./assets/writing.png",
+  title: "Branding",
+  desc:
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+  img: "https://neilpatel.com/wp-content/uploads/2021/08/social-media-metrics.jpg",
+},
+];
+
+const handleClick =(way) =>{
+
+
+  way === "left" ? setCurrentSlide(currentSlide > 0  ? currentSlide -1 : 2) :
+  setCurrentSlide(currentSlide <data.length -1? currentSlide +1 :0);
+}
+
+
+
   return (
     <div className="works" id="works">
-      <div className="slider">
+      <div className="slider" style={{transform:`translateX(-${currentSlide *100}vw)` }}>
+        {data.map((d) => (
         <div className="container">
           <div className="item">
             <div className="left">
               <div className="leftContainer">
                 <div className="imgContainer">
-                  <img src="assets/mobile.png" alt="" />
+                  <img src={d.icon} alt="" />
                 </div>
-                <h2>Title</h2>
+                <h2>{d.title}</h2>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-                  urna risus, condimentum suscipit sapien mmodo.
-                  .
+                 {d.desc}
                 </p>
                 <span>Projects</span>
               </div>
             </div>
-            <div className="right"> </div>
+            <div className="right"> 
+            
+
+            <img src= "https://miro.medium.com/max/1024/1*GaBtlHe240ZkwlcBrFczgQ.jpeg" alt="" />
+            
+            </div>
           </div>
-        </div>
+          
+        </div> 
+        ))}
       </div>
+        
+      <img src="assets/arrow.png" className= "arrow left" alt= "" onClick={() => handleClick("left")} />
+      <img src="assets/arrow.png" className= "arrow right" alt= ""  onClick={() => handleClick()}/>
     </div>
   );
 }
