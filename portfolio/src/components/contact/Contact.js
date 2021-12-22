e81b97ecd0b1559d2fd11d7317edfd63637b96d0
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2021-12-22 15:13:53 -0500

    added a form submission function

--- portfolio/src/components/contact/Contact.js
+++ portfolio/src/components/contact/Contact.js
@@ -1,13 +1,17 @@
 
    
 import { useState } from "react";
+import emailjs from 'emailjs-com';
 import "./contact.scss";
 
 export default function Contact() {
   const [message, setMessage] = useState(false);
 
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    emailjs.sendForm("service_hgh6di9","template_m5cazjo",e.target,  "user_Gd9cR6GwJni38Y62QDpP3"  );
     setMessage(true);
   };
   return (
@@ -18,8 +22,8 @@ export default function Contact() {
       <div className="right">
         <h2>Contact.</h2>
         <form onSubmit={handleSubmit}>
-          <input type="text" id = "email" placeholder="Email" />
-          <textarea  id= "message" placeholder="Message"></textarea>
+          <input type="text" name = "email" placeholder="Email" />
+          <textarea  name= "message" placeholder="Message"></textarea>
           <button type="submit">Send</button>
           {message && <span>Thanks, I'll reply ASAP!</span>}
         </form>
