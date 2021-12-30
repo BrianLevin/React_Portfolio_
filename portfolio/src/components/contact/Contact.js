eb6a87c9e2ca0c3dac42cfd8c0ab31525709a95f
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2021-12-30 15:30:53 -0500

    note

--- portfolio/src/components/contact/Contact.js
+++ portfolio/src/components/contact/Contact.js
@@ -13,15 +13,26 @@ export default function Contact() {
     e.preventDefault();
     emailjs.sendForm("service_hgh6di9","template_m5cazjo",e.target,  "user_Gd9cR6GwJni38Y62QDpP3"  );
     setMessage(true);
+    
   };
+ 
+  
   return (
     <div className="contact" id="contact">
       <div className="left">
+        <p>Yes, you can do this in react.
+
+
+
+
+
+5:56
+If you'd prefer to use a react hook, I think useRef() would work as well (but I think that would require more code)</p>
         <img src="assets/shake.svg" alt="" />
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} >
           <input type="text" name = "email" placeholder="Email" />
           <textarea  name= "message" placeholder="Message"></textarea>
           <button type="submit">Send</button>
